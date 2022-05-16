bcfd9cc2e9b8de826f0f47e644a14fc48b64e247
heatflat1021 <heatflat1021@gmail.com>
2022-05-16 22:00:56 +0900

    modified so that all required BandPower values are available in the MainBoard.

--- Assets/Scripts/MainBoard.cs
+++ Assets/Scripts/MainBoard.cs
@@ -13,7 +13,15 @@ namespace dirox.emotiv.controller
         DataStreamManager _dataStreamMgr = DataStreamManager.Instance;
 
         float _timerDataUpdate = 0;
-        const float TIME_UPDATE_DATA = 1f;
+        const float TIME_UPDATE_DATA = 0.3f;
+
+        private readonly List<Channel_t> BandPowerTargetChannel = new List<Channel_t> {
+            Channel_t.CHAN_AF3, Channel_t.CHAN_F7, Channel_t.CHAN_F3, Channel_t.CHAN_FC5, // 左前方
+            Channel_t.CHAN_T7, Channel_t.CHAN_P7, Channel_t.CHAN_O1, // 左後方
+            Channel_t.CHAN_AF4, Channel_t.CHAN_F8, Channel_t.CHAN_F4, Channel_t.CHAN_FC6, // 右前方
+            Channel_t.CHAN_T8, Channel_t.CHAN_P8, Channel_t.CHAN_O2, // 右後方
+        };
+
 
         void Update()
         {
@@ -24,23 +32,23 @@ namespace dirox.emotiv.controller
 
             _timerDataUpdate += Time.deltaTime;
             if (_timerDataUpdate < TIME_UPDATE_DATA)
+            {
                 return;
-
+            }
             _timerDataUpdate -= TIME_UPDATE_DATA;
 
-            Debug.Log(_dataStreamMgr.GetEEGChannels().Count);
-            // update power band
 
-            Debug.Log(_dataStreamMgr.GetBandPowerLists().ToString());
-            Debug.Log(_dataStreamMgr.GetBandPowerLists().Count);
-
-            if (_dataStreamMgr.GetNumberEEGSamples() > 0)
+            if (_dataStreamMgr.GetNumberPowerBandSamples() > 0)
             {
-                foreach (var channel in DataStreamManager.Instance.GetEEGChannels())
+                List<Channel_t> list = DataStreamManager.Instance.GetEEGChannels();
+                foreach (var channel in BandPowerTargetChannel)
                 {
-                    Debug.Log(channel);
-                    double theta = DataStreamManager.Instance.GetAlphaData(channel);
-                    Debug.Log(theta);
+                    double theta = DataStreamManager.Instance.GetThetaData(channel);
+                    double alpha = DataStreamManager.Instance.GetAlphaData(channel);
+                    double lowBeta = DataStreamManager.Instance.GetLowBetaData(channel);
+                    double highBeta = DataStreamManager.Instance.GetHighBetaData(channel);
+
+                    Debug.Log($"[{channel}] t:{theta}, a:{alpha}, l:{lowBeta}, h:{highBeta}");
                 }
             }
         }
